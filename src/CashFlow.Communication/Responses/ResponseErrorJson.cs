14d2e214f8f1e51e12ec5284129d3384785e8c14
thaynaradamazio <thaydamazio2015@gmail.com>
2024-12-17 14:19:36 -0300

    Initial commit

---
+++ src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,13 @@
+﻿namespace CashFlow.Communication.Responses
+{
+    public class ResponseErrorJson
+    {
+        public string ErrorMessage { get; set; } = string.Empty;
+
+
+        public ResponseErrorJson(string errorMessage) 
+        { 
+            ErrorMessage = errorMessage;
+        }
+    }
+}
