e8aa966edff93ce1e6cd7e5600dc76ee0f9fce95
Thaynara <thaydamazio2015@gmail.com>
2025-02-10 22:42:37 -0300

    feat: add exception for id not found

--- src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlow.Application.UseCases.Expenses.GetById
 {
@@ -18,6 +20,11 @@ namespace CashFlow.Application.UseCases.Expenses.GetById
         public async Task<ResponseExpenseJson> Execute(long id)
         {
             var result = await _repository.GetById(id);
+            if(result is null)
+            {
+                throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
+            }
+
             return _mapper.Map<ResponseExpenseJson>(result);
         }
     }
---
+++ src/CashFlow.Exception/ExceptionsBase/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace CashFlow.Exception.ExceptionsBase
+{
+    public class NotFoundException : CashFlowException
+    {
+        public NotFoundException(string message) : base(message)
+        { 
+            
+        }
+    }
+}
