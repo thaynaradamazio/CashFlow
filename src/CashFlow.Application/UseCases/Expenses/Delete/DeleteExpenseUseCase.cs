da3244bd3e7a37922cdee43a615918fd609276b5
Thaynara <thaydamazio2015@gmail.com>
2025-02-13 18:15:33 -0300

    feat: delete expense

---
+++ src/CashFlow.Application/UseCases/Expenses/Delete/DeleteExpenseUseCase.cs
@@ -0,0 +1,33 @@
+﻿using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Domain.Repositories;
+using CashFlow.Exception.ExceptionsBase;
+using CashFlow.Exception;
+
+namespace CashFlow.Application.UseCases.Expenses.Delete
+{
+    public class DeleteExpenseUseCase : IDeleteExpenseUseCase
+    {
+        private readonly IExpensesWriteOnlyRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteExpenseUseCase(
+        IExpensesWriteOnlyRepository repository,
+        IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Execute(long id)
+        {
+            var result = await _repository.Delete(id);
+
+            if (result == false)
+            {
+                throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
+            }
+
+            await _unitOfWork.Commit();
+        }
+    }
+}
