efab9ea844f7d90d3245a8a10753cdba80a05b24
Thaynara <thaydamazio2015@gmail.com>
2025-02-20 00:12:06 -0300

    feat: add initial class to generate expense report excel

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpenseReportExcelUseCase.cs
@@ -0,0 +1,18 @@
+﻿using ClosedXML.Excel;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Excel
+{
+    public class GenerateExpenseReportExcelUseCase : IGenerateExpenseReportExcelUseCase
+    {
+        public async Task<byte[]> Execute(DateOnly month)
+        {
+            var workbook = new XLWorkbook();
+
+            workbook.Author = "thay damazio";
+            workbook.Style.Font.FontSize = 12;
+            workbook.Style.Font.FontName = "Times New Roman";     
+           
+            var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
+        }
+    }
+}
