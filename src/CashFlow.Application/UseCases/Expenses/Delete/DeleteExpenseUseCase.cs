0fa28975bf5b78a97fe0bc5217e53239adddc635
thaynaradamazio <thaydamazio2015@gmail.com>
2025-03-19 13:50:30 -0300

    fix: fix all expenses endpoints

--- src/CashFlow.Application/UseCases/Expenses/Delete/DeleteExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Delete/DeleteExpenseUseCase.cs
@@ -2,30 +2,41 @@
 using CashFlow.Domain.Repositories;
 using CashFlow.Exception.ExceptionsBase;
 using CashFlow.Exception;
+using CashFlow.Domain.Services.LoggedUser;
 
 namespace CashFlow.Application.UseCases.Expenses.Delete
 {
     public class DeleteExpenseUseCase : IDeleteExpenseUseCase
     {
+        private readonly IExpensesReadOnlyRepository _expensesReadOnly;
         private readonly IExpensesWriteOnlyRepository _repository;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly ILoggedUser _loggedUser;
 
         public DeleteExpenseUseCase(
+        IExpensesReadOnlyRepository expensesReadOnly,
         IExpensesWriteOnlyRepository repository,
-        IUnitOfWork unitOfWork)
+        IUnitOfWork unitOfWork,
+        ILoggedUser loggedUser)
         {
             _repository = repository;
             _unitOfWork = unitOfWork;
+            _loggedUser = loggedUser;
+            _expensesReadOnly = expensesReadOnly;
         }
 
         public async Task Execute(long id)
         {
-            var result = await _repository.Delete(id);
+            var loggedUser = await _loggedUser.Get();
 
-            if (result == false)
+            var expense = await _expensesReadOnly.GetById(loggedUser, id);
+
+            if (expense is null)
             {
                 throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
             }
+
+            await _repository.Delete(id);
 
             await _unitOfWork.Commit();
         }
--- src/CashFlow.Application/UseCases/Expenses/GetAll/GetAllExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/GetAll/GetAllExpenseUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Domain.Services.LoggedUser;
 
 namespace CashFlow.Application.UseCases.Expenses.GetAll
 {
@@ -8,14 +9,18 @@ namespace CashFlow.Application.UseCases.Expenses.GetAll
     {
         private readonly IExpensesReadOnlyRepository _repository;
         private readonly IMapper _mapper;
-        public GetAllExpenseUseCase(IExpensesReadOnlyRepository repository, IMapper mapper) 
+        private readonly ILoggedUser _loggedUser;
+        public GetAllExpenseUseCase(IExpensesReadOnlyRepository repository, IMapper mapper, ILoggedUser loggedUser) 
         {
             _repository = repository;
             _mapper = mapper;
+            _loggedUser = loggedUser;
         }
         public async Task<ResponseExpensesJson> Execute()
         {
-            var result = await _repository.GetAll();
+            var loggedUser = await _loggedUser.Get();
+            
+            var result = await _repository.GetAll(loggedUser);
 
             return new ResponseExpensesJson
             {
