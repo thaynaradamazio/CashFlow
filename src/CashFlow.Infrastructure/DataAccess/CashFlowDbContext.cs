2e0bd71f6545afb9bfd860221965387510367355
thaynaradamazio <thaydamazio2015@gmail.com>
2025-04-04 13:21:05 -0300

    feat: add table tags

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -9,5 +9,12 @@ namespace CashFlow.Infrastructure.DataAccess
 
         public DbSet<Expense> Expenses { get; set; }
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Tag>().ToTable("Tags");
+        }
     }
 }
